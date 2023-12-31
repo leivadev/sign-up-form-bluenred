11a905b9ee3dfdf4bbf5502a5a82cd4e3a90471b
Luislev <luis.leiva@utp.ac.pa>
2023-12-31 01:05:19 -0500

    add theme switch button function and style

--- script.js
+++ script.js
@@ -90,3 +90,32 @@ confirmPassword.addEventListener('input', function (e) {
 });
 
 //Dark and light mode
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+const button = document.querySelector("[data-theme-toggle]");
+const iconElement = document.getElementById("icon");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+
+    if (systemSettingDark.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+button.addEventListener("click", () => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+    const newIcon = newTheme === "dark" ? "fa-solid fa-moon" : "fa-solid fa-sun";
+    iconElement.className = newIcon;
+    document.querySelector("html").setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    currentThemeSetting = newTheme;
+});
